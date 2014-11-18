245bddfd6b434a0a9ff83d2137c886b48e16ec22
madhukaraphatak <madhukaraphatak@gmail.com>
2014-11-18 13:44:33 +0530

    renamed the project

--- code/kryoexample/src/main/scala/com/madhu/spark/kryo/KryoExample.scala
+++ code/kryoexample/src/main/scala/com/madhu/spark/kryo/KryoExample.scala
@@ -2,13 +2,12 @@ package com.madhu.spark.kryo
 
 import java.io.ByteArrayOutputStream
 
-import com.esotericsoftware.kryo.io.{Input, Output}
+import com.esotericsoftware.kryo.io.Input
 import org.apache.hadoop.io.{BytesWritable, NullWritable}
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.KryoSerializer
-import org.objenesis.strategy.StdInstantiatorStrategy
+import org.apache.spark.{SparkConf, SparkContext}
 
 import scala.reflect.ClassTag
 
@@ -19,6 +18,27 @@ import scala.reflect.ClassTag
 
 object KryoExample {
 
+  def main(args: Array[String]) {
+
+    if (args.length < 1) {
+      println("Please provide output path")
+      return
+    }
+    val outputPath = args(0)
+
+    val conf = new SparkConf().setMaster("local").setAppName("kryoexample")
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+    val sc = new SparkContext(conf)
+
+    //create some dummy data
+    val personList = 1 to 10000 map (value => new Person(value + ""))
+    val personRDD = sc.makeRDD(personList)
+
+    saveAsObjectFile(personRDD, outputPath)
+    val rdd = objectFile[Person](sc, outputPath)
+    println(rdd.map(person => person.name).collect().toList)
+  }
+
   /*
    * Used to write as Object file using kryo serialization
    */
@@ -27,14 +47,15 @@ object KryoExample {
 
     rdd.mapPartitions(iter => iter.grouped(10)
       .map(_.toArray))
-      .map(x => {
-      //initializes kyro and calls your registrator clas
+      .map(splitArray => {
+      //initializes kyro and calls your registrator class
       val kryo = kryoSerializer.newKryo()
 
       //convert data to bytes
       val bao = new ByteArrayOutputStream()
-      val output = new Output(bao)
-      kryo.writeClassAndObject(output, x)
+      val output = kryoSerializer.newKryoOutput()
+      output.setOutputStream(bao)
+      kryo.writeClassAndObject(output, splitArray)
       output.close()
 
       // We are ignoring key field of sequence file
@@ -52,12 +73,7 @@ object KryoExample {
     sc.sequenceFile(path, classOf[NullWritable], classOf[BytesWritable], minPartitions)
       .flatMap(x => {
       val kryo = kryoSerializer.newKryo()
-      // using StdInstantiatorStrategy to skip requirement of default constructor in custom classes
-      // refer https://github.com/EsotericSoftware/kryo#object-creation
-      kryo.setInstantiatorStrategy(new StdInstantiatorStrategy())
       val input = new Input()
-      kryo.setClassLoader(Option(Thread.currentThread().getContextClassLoader)
-        .getOrElse(getClass.getClassLoader))
       input.setBuffer(x._2.getBytes)
       val data = kryo.readClassAndObject(input)
       val dataObject = data.asInstanceOf[Array[T]]
@@ -68,26 +84,5 @@ object KryoExample {
 
   // user defined class that need to serialized
   class Person(val name: String)
-
-  def main(args: Array[String]) {
-
-    if (args.length < 1) {
-      println("Please provide output path")
-      return
-    }
-    val outputPath = args(0)
-
-    val conf = new SparkConf().setMaster("local").setAppName("test")
-    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    val sc = new SparkContext(conf)
-
-    //create some dummy data
-    val personList = 1 to 10000 map (value => new Person(value + ""))
-    val personRDD = sc.makeRDD(personList)
-
-    saveAsObjectFile(personRDD, outputPath)
-    val rdd = objectFile[Person](sc, outputPath)
-    println(rdd.map(person => person.name).collect().toList)
-  }
 
 }
